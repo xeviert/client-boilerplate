0ee528b5a74ec607073defb853b8fb4991234553
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-02-07 22:51:52 -0600

    Added Context

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Home from './Components/Home'
 import Users from './Components/Users'
 import Footer from './Components/Footer'
 
+import AppProvider from './AppContext'
+
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/users" component={Users} />
-          <Route path="/" exact component={Home} />
-        </Switch>
-      <Footer />
-    </Router>
+    <AppProvider>
+        <Router>
+          <NavBar />
+            <Switch>
+              <Route path="/about" component={About} />
+              <Route path="/users" component={Users} />
+              <Route path="/" exact component={Home} />
+            </Switch>
+          <Footer />
+        </Router>
+    </AppProvider>
   );
 }
 
